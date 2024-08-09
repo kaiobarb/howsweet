db52b70517a41b41d15a4fa555e50687e1c91440
Kaio <kaiobarb@gmail.com>
2024-08-08 18:29:11 -0700

    remove bad page

--- src/app/play/components/game-state.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { GameState } from "@/lib/types";
-import { useSearchParams } from "next/navigation";
-const GameSession = ({ productId }: GameState) => {
-  const searchParams = useSearchParams();
-};
